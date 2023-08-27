b565a593cbf072ee56019cc6642fb255eb282e14
fcbarros <fabio.c.barros@gmail.com>
2023-08-27 17:40:42 +0200

    fix: docker build

--- docker/build-ros2.sh
+++ docker/build-ros2.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 version=1.0.5
-docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile . -t bobcamera/bob-ros2-dev:$version -t bobcamera/bob-ros2-dev:latest --target bob-ros2-dev
+docker buildx build --load --platform linux/amd64 -f Dockerfile . -t bobcamera/bob-ros2-dev:$version -t bobcamera/bob-ros2-dev:latest --target bob-ros2-dev
+#docker buildx build --push --platform linux/amd64,linux/arm64 -f Dockerfile . -t bobcamera/bob-ros2-dev:$version -t bobcamera/bob-ros2-dev:latest --target bob-ros2-dev
