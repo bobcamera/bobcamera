6da11fe0bd05987c5033e1a3c42e545e1944aa0f
Mike <33353907+Micky-G@users.noreply.github.com>
2023-08-29 18:18:54 +0100

    Record messages to a rosbag (#1)
    * Adding the ability to record messages to a rosbag
    
    * Tidying up interface types
    
    * Remove duplicate node specification
    
    * Remove the recorder node from launch
    
    * remove some unwanted files

---
+++ src/ros2/src/bob_recorder/bob_recorder/rosbag_recorder.py
@@ -0,0 +1,47 @@
+import rclpy.logging
+from rclpy.time import Time
+from datetime import datetime
+from rclpy.serialization import serialize_message
+import rosbag2_py
+from sensor_msgs.msg import Image
+from bob_interfaces.msg import TrackingState, TrackDetectionArray, TrackTrajectoryArray
+
+class RosbagRecorder():
+
+  def __init__(self):
+    self.logger = rclpy.logging.get_logger('rosbag_recorder')
+
+    # setup the rosbag so that we can record into it
+    db_uri = f'recordings/rosbags/{datetime.now().strftime("%Y_%m_%d-%H_%M_%S")}'
+    self.logger.info(f'Creating database {db_uri}.')
+
+    self.writer = rosbag2_py.SequentialWriter()
+    
+    storage_options = rosbag2_py._storage.StorageOptions(uri=db_uri, storage_id='sqlite3')
+    converter_options = rosbag2_py._storage.ConverterOptions('', '')
+    self.writer.open(storage_options, converter_options)
+
+    image_topic_info = rosbag2_py._storage.TopicMetadata(name='bob/camera/all_sky/bayer', type='sensor_msgs/msg/Image', serialization_format='cdr')
+    tracking_state_topic_info = rosbag2_py._storage.TopicMetadata(name='bob/tracker/tracking_state', type='bob_interfaces/msg/TrackingState', serialization_format='cdr')
+    detection_topic_info = rosbag2_py._storage.TopicMetadata(name='bob/tracker/detections', type='bob_interfaces/msg/TrackDetectionArray', serialization_format='cdr')
+    trajectory_topic_info = rosbag2_py._storage.TopicMetadata(name='bob/tracker/trajectory', type='bob_interfaces/msg/TrackTrajectoryArray', serialization_format='cdr')
+    prediction_topic_info = rosbag2_py._storage.TopicMetadata(name='bob/tracker/prediction', type='bob_interfaces/msg/TrackTrajectoryArray', serialization_format='cdr')
+
+    self.writer.create_topic(image_topic_info)
+    self.writer.create_topic(detection_topic_info)
+    self.writer.create_topic(tracking_state_topic_info)
+    self.writer.create_topic(trajectory_topic_info)
+    self.writer.create_topic(prediction_topic_info)
+
+  def record(self, masked_frame:Image, msg_tracking_state:TrackingState, msg_detection_array:TrackDetectionArray, 
+    msg_trajectory_array:TrackTrajectoryArray, msg_prediction_array:TrackTrajectoryArray):
+     
+    ns = Time.from_msg(masked_frame.header.stamp).nanoseconds
+    self.writer.write('bob/camera/all_sky/bayer', serialize_message(masked_frame), ns)
+    self.writer.write('bob/tracker/tracking_state', serialize_message(msg_tracking_state), ns)
+    self.writer.write('bob/tracker/detections', serialize_message(msg_detection_array), ns)
+    self.writer.write('bob/tracker/trajectory', serialize_message(msg_trajectory_array), ns)
+    self.writer.write('bob/tracker/prediction', serialize_message(msg_prediction_array), ns)
+
+    status_message = f"(BoB) Tracker Status: trackable:{msg_tracking_state.trackable}, alive:{msg_tracking_state.alive}, started:{msg_tracking_state.started}, ended:{msg_tracking_state.ended}"
+    self.logger.debug(status_message)
