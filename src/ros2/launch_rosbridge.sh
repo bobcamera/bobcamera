525f9c51a4f407599c9128496620dfb61d68c6ce
Mike <33353907+Micky-G@users.noreply.github.com>
2023-09-11 17:56:09 +0100

    Gui edits (#21)
    Co-authored-by: Dan <danlohman@users.noreply.github.com>

---
+++ src/ros2/launch_rosbridge.sh
@@ -0,0 +1,3 @@
+    # Navigate to the specified directory and run the rosbridge_server
+    cd /workspaces/bobcamera/src/ros2/src/rosbridge_suite/rosbridge_server/launch
+    ros2 run rosbridge_server rosbridge_websocket
