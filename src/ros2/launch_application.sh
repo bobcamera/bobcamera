1f5f0b0f94ed78df68004e03d5f91987db98e214
Mike <michael@mwgtechnologies.co.uk>
2023-12-31 13:20:59 +0000

    rollback launch changes

--- src/ros2/launch_application.sh
+++ src/ros2/launch_application.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # source options are "'rtsp'", "'usb'", "'video'", "'simulate'", "'rtsp_overlay'", "'video_overlay'"
-export BOB_SOURCE=${BOB_SOURCE:-"'rtsp'"}
+export BOB_SOURCE=${BOB_SOURCE:-"'video_overlay'"}
 export BOB_RTSP_URL=${BOB_RTSP_URL:-"rtsp://sky360:Sky360Sky!@10.20.30.75:554/cam/realmonitor?channel=1&subtype=0"}
-export BOB_RTSP_WIDTH=${BOB_RTSP_WIDTH:-"3840"}
-export BOB_RTSP_HEIGHT=${BOB_RTSP_HEIGHT:-"2160"}
+export BOB_RTSP_WIDTH=${BOB_RTSP_WIDTH:-"1920"}
+export BOB_RTSP_HEIGHT=${BOB_RTSP_HEIGHT:-"1080"}
+#export BOB_RTSP_URL=${BOB_RTSP_URL:-"rtsp://sky360:Sky360Sky!@10.20.30.75:554/cam/realmonitor?channel=1&subtype=0"}
+#export BOB_RTSP_WIDTH=${BOB_RTSP_WIDTH:-"3840"}
+#export BOB_RTSP_HEIGHT=${BOB_RTSP_HEIGHT:-"2160"}
 #export BOB_RTSP_URL=${BOB_RTSP_URL:-"rtsp://bob:Sky360Sky!@10.20.30.58:554/Streaming/Channels/101"}
 #export BOB_RTSP_WIDTH=${BOB_RTSP_WIDTH:-"2560"}
 #export BOB_RTSP_HEIGHT=${BOB_RTSP_HEIGHT:-"2560"}
+
 export BOB_CAMERA_ID=${BOB_CAMERA_ID:-"0"}
 export BOB_ENABLE_VISUALISER=${BOB_ENABLE_VISUALISER:-"True"}
 export BOB_OPTIMISED=${BOB_OPTIMISED:-"True"}
@@ -27,8 +31,8 @@ export BOB_TRACKING_USEMASK=${BOB_TRACKING_USEMASK:-"False"}
 export BOB_TRACKING_MASK_FILE=${BOB_TRACKING_MASK_FILE:-"assets/masks/mask.jpg"}
 
 # simulation
-export BOB_SIMULATION_WIDTH=${BOB_SIMULATION_WIDTH:- "3840"}
-export BOB_SIMULATION_HEIGHT=${BOB_SIMULATION_HEIGHT:- "2160"}
+export BOB_SIMULATION_WIDTH=${BOB_SIMULATION_WIDTH:- "1920"}
+export BOB_SIMULATION_HEIGHT=${BOB_SIMULATION_HEIGHT:- "1080"}
 export BOB_SIMULATION_NUM_OBJECTS=${BOB_SIMULATION_NUM_OBJECTS:- "5"}
 
 ./launcher.sh application_launch.py
