02c428f003d89d0528684f351b086a5dc02e1abf
Mike <michael@mwgtechnologies.co.uk>
2024-02-02 18:17:44 +0000

    Validating incremental zoom algorithm feature enhancement within the ptz raster dummy

--- src/ros2/src/bob_ptz/bob_ptz/ptz_raster_v1_dummy_code.py
+++ src/ros2/src/bob_ptz/bob_ptz/ptz_raster_v1_dummy_code.py
@@ -2,6 +2,10 @@ import math
 import time
 
 
+
+
+
+
 #For testing Purposes
 Rasterstep = 0
 completeMSG = False 
@@ -21,17 +25,20 @@ initiate_v1 = True
 while(completeMSG == False):
     if( (initiate_v1 == True)  or  (RasterImageACK_v1 == True)):
         #initially magic numbers, later ros-msg-values:
-        startX = -1; endX = -0.6; startY = -1; endY = -0.4
-        stepwidthX = 0.2; stepwidthY = 0.2
-        XIncrementsPerY = math.ceil(abs(startX-endX)/stepwidthX)+1
+        startX = -1; endX = -0.6; startY = -1; endY = -0.4; startZoom = 0; endZoom = 1
+        stepwidthX = 0.2; stepwidthY = 0.2; stepwidthZoom = 0.2
+        ZoomIncrementsPerX = math.ceil(abs(startZoom-endZoom)/stepwidthZoom)+1
+        XIncrementsPerYmultiplesOfZoom = math.ceil(ZoomIncrementsPerX*(abs(startX-endX)/stepwidthX)+1)
         YStepsTotal = math.ceil(abs(startY-endY)/stepwidthY)+1
-        if(Rasterstep < XIncrementsPerY*YStepsTotal):
-            currentStepX = round(startX + (stepwidthX*(Rasterstep % XIncrementsPerY)),10)
-            currentStepY = round(startY + (stepwidthY*(math.floor(Rasterstep  / XIncrementsPerY))),10)
+        currentZoom = startZoom
+        if(Rasterstep < XIncrementsPerYmultiplesOfZoom*YStepsTotal):
+            currentZoom = round(startZoom+(stepwidthZoom*(Rasterstep % ZoomIncrementsPerX )),10)
+            currentStepX = round(startX + (stepwidthX*(Rasterstep % (XIncrementsPerYmultiplesOfZoom % ZoomIncrementsPerX) )),10)
+            currentStepY = round(startY + (stepwidthY*(math.floor(Rasterstep  / XIncrementsPerYmultiplesOfZoom / ZoomIncrementsPerX))),10)
             #Polling ONVIF if target has been reached
             #while((ONVIFGetprofile.X != currentStepX) and (ONVIFGetprofile.Y != currentStepY)):
                 #time.sleep(0.1)
-            print(f"Ros2 MSG calibrate/v1, CurrentStepX: {currentStepX}, CurrentStepY: {currentStepY}")
+            print(f"Ros2 MSG calibrate/v1, CurrentStepX: {currentStepX}, CurrentStepY: {currentStepY}, CurrentZoom: {currentZoom}")
         else:
             completeMSG = True
             print("ROS2 MSG bob/ptz/calibrate/v1/complete.msg ACK")
