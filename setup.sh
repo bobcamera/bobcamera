ea9da91259215d0b221d4b12f48cffd98e02625f
Michael Groenewald <michael.groenewald@gmail.com>
2024-02-19 11:12:07 +0000

    Made some changes to the setup.sh file to check if docker and compose is installed before installing

--- setup.sh
+++ setup.sh
@@ -5,26 +5,38 @@ set -o nounset
 IFS=$(printf '\n\t')
 
 # Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-printf '\nDocker installed successfully\n\n'
 
-printf 'Waiting for Docker to start...\n\n'
-sleep 5
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    echo 'Docker is already installed, skipping.'
+else
+    echo 'Docker is not installed, installing...\n\n'
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    printf '\nDocker installed successfully\n\n'
 
-printf 'Adding user to the docker group...\n\n'
-sudo usermod -aG docker $USER
+    printf 'Waiting for Docker to start...\n\n'
+    sleep 5
 
-printf 'Installing Docker Compose...\n\n'
+    printf 'Adding user to the docker group...\n\n'
+    sudo usermod -aG docker $USER
+fi
 
 # Docker Compose
-mkdir -p ~/.docker/cli-plugins/
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-curl -SL https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-printf '\nDocker Compose installed successfully\n\n'
 
-docker compose version
+# Check if Docker Compose is installed
+if command -v docker compose &> /dev/null; then
+    echo 'Docker Compose is already installed, skipping.'
+else
+    echo 'Docker Compose is not installed, installing...\n\n'
+    mkdir -p ~/.docker/cli-plugins/
+    COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+    curl -SL https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+    chmod +x ~/.docker/cli-plugins/docker-compose
+    printf '\nDocker Compose installed successfully\n\n'
+fi
+
+#docker compose version
 
 #UID=${UID} GID=${GID} docker-compose build
 #UID=${UID} GID=${GID} docker-compose up
