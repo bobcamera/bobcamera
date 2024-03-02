cfc0ece5989156fc3aa5d35771cc14bcb8a17222
Michael Groenewald <michael.groenewald@gmail.com>
2024-03-02 23:25:54 +0000

    changes to support dual masks

--- src/web2/src/sidebar.php
+++ src/web2/src/sidebar.php
@@ -30,6 +30,14 @@
             Foreground Mask
         </a>
     </li>
+</ul>
+<hr class="my-3">
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-1 mb-1 text-body-secondary text-uppercase">
+    <span>Mask Control</span>
+    <a class="link-secondary" href="#" aria-label="">
+    </a>
+</h6>
+<ul class="nav flex-column">
     <li class="nav-item">
         <a class="nav-link d-flex align-items-center gap-2" href="iframe.php?file=html/2D-full-width-stream-security-mask.html">
         <i class="bi bi-camera"></i>
