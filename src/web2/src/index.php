b5fb0727f1840b012bd431db83a54ea4f2dba152
Michael Groenewald <michael.groenewald@gmail.com>
2024-03-03 18:55:32 +0000

    changef titles from Bob to BOB the Universal Object Tracker

--- src/web2/src/index.php
+++ src/web2/src/index.php
@@ -12,7 +12,7 @@ $time = $_GET['time'] ?? '1705000021';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <title>Bob Player</title>
+    <title>BOB the Universal Object Tracker</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link href="lib/bootstrap/bootstrap.min.css" rel="stylesheet">
@@ -215,7 +215,7 @@ $time = $_GET['time'] ?? '1705000021';
     </svg>
 
     <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Bob</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">BOB the Universal Object Tracker</a>
 
         <ul class="navbar-nav flex-row d-md-none">
             <li class="nav-item text-nowrap">
@@ -244,7 +244,7 @@ $time = $_GET['time'] ?? '1705000021';
             <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
                 <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
                     <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="sidebarMenuLabel">Bob</h5>
+                        <h5 class="offcanvas-title" id="sidebarMenuLabel">BOB the Universal Object Tracker</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
