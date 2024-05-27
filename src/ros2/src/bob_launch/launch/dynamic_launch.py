7f48f8512c96b1dbfc3e6da4760eb7a798a5ccea
fcbarros <fabio.c.barros@gmail.com>
2024-05-27 17:20:54 +0200

    Adding standalone python nodes to dynamic

--- src/ros2/src/bob_launch/launch/dynamic_launch.py
+++ src/ros2/src/bob_launch/launch/dynamic_launch.py
@@ -3,7 +3,7 @@ import launch
 import yaml
 from launch import LaunchDescription
 from launch.actions import LogInfo
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import Node, ComposableNodeContainer
 from launch.actions import DeclareLaunchArgument
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import PythonExpression, LaunchConfiguration, EnvironmentVariable 
@@ -18,7 +18,7 @@ def parse_yaml(file_path):
         print(f"Error loading YAML file: {e}")
         return None
 
-def generate_composable_nodes(config, node_container, namespace, config_file):
+def generate_composable_nodes(config, node_container, namespace):
     node_descriptions = node_container['nodes']
     nodes = []
     for node_description in node_descriptions:
@@ -34,7 +34,7 @@ def generate_composable_nodes(config, node_container, namespace, config_file):
             plugin=node_description['plugin'],
             name=node_description['name'],
             namespace=namespace,
-            parameters = [parameters],
+            parameters=[parameters],
             extra_arguments=[{'use_intra_process_comms': True}],
             remappings=remappings
         )
@@ -42,23 +42,14 @@ def generate_composable_nodes(config, node_container, namespace, config_file):
     return nodes
 
 
-def generate_launch_description():
-    config_file_path = 'assets/config/video_simulator_config.yaml'
-    namespace = EnvironmentVariable('BOB_NAMESPACE', default_value="")
-    loglevel = EnvironmentVariable('BOB_LOGLEVEL', default_value="INFO")
-    # print(f"config_file: {config_file_path}, namespace: {namespace}, loglevel: {loglevel}")
-
-    config = parse_yaml(config_file_path)
-    if config is None:
-        raise RuntimeError("Failed to load YAML configuration")
-
+def generate_containers(config, namespace, loglevel):
     containers = []
     node_containers = config['launch']['node_containers']
     for node_container in node_containers:
         node_container_name = node_container['name']
         print(f"Container Name: {node_container_name}")
 
-        composable_nodes = generate_composable_nodes(config, node_container, namespace, config_file_path)
+        composable_nodes = generate_composable_nodes(config, node_container, namespace)
 
         container = None
         try:
@@ -78,5 +69,52 @@ def generate_launch_description():
             raise RuntimeError("Failed to create ComposableNodeContainer")
 
         containers.append(container)
+    
+    return containers
+
+def generate_nodes(config, namespace, loglevel):
+    nodes = []
+    config_nodes = config['launch']['nodes']
+    for config_node in config_nodes:
+        remappings = [
+            (k, v) for mapping in config_node.get('remappings', [])
+            for k, v in mapping.items()
+        ]
+        parameters = config[config_node['name']]['ros__parameters']
+        print(f"Standalone Node: Package: {config_node['package']}, Executable: {config_node['executable']}, Name: {config_node['name']}")
+
+        node = None
+        try:
+            node = Node(
+                package=config_node['package'],
+                namespace=namespace,
+                executable=config_node['executable'],
+                name=config_node['name'],
+                arguments=['--ros-args', '--log-level', loglevel],
+                parameters=[parameters],
+                remappings=remappings,
+            )
+        except Exception as e:
+            print(f"Error creating Standalone Node: {e}")
+        
+        if node is None:
+            raise RuntimeError("Failed to create Standalone Node")
+
+        nodes.append(node)
+    
+    return nodes
+
+
+def generate_launch_description():
+    config_file_path = 'assets/config/video_simulator_config.yaml'
+    namespace = EnvironmentVariable('BOB_NAMESPACE', default_value="")
+    loglevel = EnvironmentVariable('BOB_LOGLEVEL', default_value="INFO")
+
+    config = parse_yaml(config_file_path)
+    if config is None:
+        raise RuntimeError("Failed to load YAML configuration")
+
+    containers = generate_containers(config, namespace, loglevel)
+    containers.extend(generate_nodes(config, namespace, loglevel))
 
     return LaunchDescription(containers)
