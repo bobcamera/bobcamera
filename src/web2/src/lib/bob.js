5e6023a6246ff8524e13bcf3ed8622a1eabb9175
Mike <33353907+Micky-G@users.noreply.github.com>
2024-05-30 22:39:06 +0100

    Attempting to fix mask connection issues (#197)

--- src/web2/src/lib/bob.js
+++ src/web2/src/lib/bob.js
@@ -1,36 +1,10 @@
 // Define the Websockets address of the ROS2 environment
 var websocketsURL = 'ws://' + window.location.hostname + ':9090';
-var ros; // Global ROS connection
+//var ros; // Global ROS connection
 var subscribedTopics = [];
 
-// // Set up the connection to the ROS2 environment using the roslib.js library
-// function rosConnect(ros, websocketsURL, retry=false){
-//     if(ros) {
-//         ros.connect(websocketsURL);
-//     } else {
-//         console.log('Creating new ros connection...');
-//         var ros = new ROSLIB.Ros();
-//         ros.connect(websocketsURL);
-//     }
-//     ros.on('connection', function(){
-//         //console.log('Connected to websocket server.')
-//     });
-//     ros.on('error', function(){
-//         //console.log('Error connecting to websocket server: ', error);
-//     });
-//     ros.on('close', function(){
-//         // Try to reconnect in 5 seconds
-//         if (retry) {
-//             //console.log('Connection to websocket server closed, trying in 5 seconds.');
-//             setTimeout(function(){rosConnect(ros, websocketsURL)}, 5000);
-//         }
-//     });
-// }
-
-function rosConnect(websocketsURL, retry = false) {
-    var retryLimit = 5; // Hard-coded retry limit
-    var retryCount = 0; // Initialize retry count
-
+// Set up the connection to the ROS2 environment using the roslib.js library
+function rosConnect(ros, websocketsURL, retry=false) {
     if (!ros) {
         console.log('Creating new ros connection...');
         ros = new ROSLIB.Ros();
@@ -47,7 +21,6 @@ function rosConnect(websocketsURL, retry = false) {
         ros.on('connection', function() {
             console.log('Connected to websocket server.');
             ros.isConnected = true;
-            retryCount = 0; // Reset retry count on successful connection
         });
 
         ros.on('error', function(error) {
@@ -58,12 +31,9 @@ function rosConnect(websocketsURL, retry = false) {
         ros.on('close', function() {
             console.log('Connection to websocket server closed.');
             ros.isConnected = false;
-            if (retry && retryCount < retryLimit) {
-                retryCount++;
-                console.log(`Connection lost, attempting to reconnect... (Attempt ${retryCount} of ${retryLimit})`);
-                setTimeout(() => rosConnect(websocketsURL, retry), 5000);
-            } else if (retry) {
-                console.log('Retry limit reached, not attempting further reconnections.');
+            if (retry) {
+                console.log('Connection lost, attempting to reconnect...');
+                setTimeout(() => rosConnect(ros, websocketsURL, retry), 1000); // Retry every second
             }
         });
 
@@ -92,3 +62,30 @@ function unsubscribeTopics(subscribedTopics) {
     });
     subscribedTopics = [];
 }
+
+// // Define the Websockets address of the ROS2 environment
+// var websocketsURL = 'ws://' + window.location.hostname + ':9090';
+
+// // Set up the connection to the ROS2 environment using the roslib.js library
+// function rosConnect(ros, websocketsURL, retry=false){
+//     if(ros) {
+//         ros.connect(websocketsURL);
+//     } else {
+//         console.log('Creating new ros connection...');
+//         var ros = new ROSLIB.Ros();
+//         ros.connect(websocketsURL);
+//     }
+//     ros.on('connection', function(){
+//         //console.log('Connected to websocket server.')
+//     });
+//     ros.on('error', function(){
+//         //console.log('Error connecting to websocket server: ', error);
+//     });
+//     ros.on('close', function(){            
+//         // Try to reconnect in 5 seconds
+//         if (retry) {
+//             //console.log('Connection to websocket server closed, trying in 5 seconds.');
+//             setTimeout(function(){rosConnect(ros, websocketsURL)}, 5000);
+//         }
+//     });
+// }
