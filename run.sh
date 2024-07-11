3493c0021c021ded325d66b360110bcbbbf7ed4f
Fabio Barros <fabio.c.barros@gmail.com>
2024-07-11 11:25:46 +0200

    Fix last generation bump

--- run.sh
+++ run.sh
@@ -10,6 +10,10 @@ fi
 export MY_UID="$(id -u)" 
 export MY_GID="$(id -g)" 
 
+mkdir -p ./assets/config
+mkdir -p ./assets/recordings
+mkdir -p ./assets/masks
+mkdir -p ./assets/wsdl
 cp $1 ./temp_config.yaml
 
 while :; do 
@@ -18,15 +22,11 @@ while :; do
     if [ "$response_lower" = "default" ] || [ "$response_lower" = "d" ]; then
         echo "Running docker compose using Default option"
         docker compose --file ./docker/docker-compose.yaml up  
-            #--env-file .env  \
-            #up
         break
     elif [ "$response_lower" = "with rstudio" ] || [ "$response_lower" = "r" ]; then
         echo "Running docker compose using Excl rStudio option"
         docker compose  \
-            --file ./docker/docker-compose-rstudio.yaml  \
-            --env-file .env  \
-            up
+            --file ./docker/docker-compose-rstudio.yaml up
         break
     else
         echo "Invalid response. Please enter 'd(efault)', 't(est)' or 'h(eadless)'."
